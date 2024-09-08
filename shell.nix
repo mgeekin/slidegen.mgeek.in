973d49b249b2909238713f556974196d9b239dc7
prateekrajgautam <prateekrajgautam@gmail.com>
2024-09-09 00:41:21 +0530

    update downloadable files

--- shell.nix
+++ shell.nix
@@ -1,29 +1,25 @@
 { pkgs ? import <nixpkgs> {} }:
-#{ pkgs ? import (fetchTarball  "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05") {} }:
+# { pkgs ? import (fetchTarball  "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11") {} }:
 let
   message = "Lets Start Development";
 in 
 pkgs.mkShellNoCC {
-  buildInputs = with pkgs; [ nodejs ];
+  buildInputs = with pkgs; [ nodejs_21 ];
 
   packages = with pkgs; [
-    nodejs_22
+    nodejs_21
     nodePackages.browser-sync 
     nodePackages.nodemon 
     nodePackages.parcel
-    vscodium 
-    firefox
-    nodejs 
- #   pkgs.nodePackages.nodemon
- #   pkgs.nodePackages.browser-sync 
- #   pkgs.nodePackages.parcel
     vim 
     curl     
-  #  cinnamon.nemo
+    vscodium 
+    firefox 
+    cinnamon.nemo
   ];
 
   shellHook = ''
     cowsay ${message}
-    npm run dev & echo "y" | codium . & firefox localhost:3000 & npm run dev & firefox 127.0.0.1:5501
+    npm run dev & echo "y" | codium & firefox localhost:3000
     '';
 }
